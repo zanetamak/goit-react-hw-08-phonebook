35e845433c32a76713f001cb8be5d7b7c99b2780
zanetamak <zaneta.zawislak@gmail.com>
2024-01-07 23:31:07 +0100

    changes

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { setFilter } from '../../redux/contacts/contactsSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFilter } from '../../redux/contacts/filterSlice';
+import { selectFilter } from '../../redux/contacts/selectors';
 import { FormControl, FormLabel, Input } from '@chakra-ui/react';
 
 const Filter = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(selectFilter);
 
   const onChange = (e) => {
     const value = e.target.value.toLowerCase();
@@ -17,6 +19,7 @@ const Filter = () => {
       <Input
         type="text"
         name="filter"
+        value={filter} // Używanie filtra z Redux za pomocą useSelector
         onChange={onChange}
         placeholder="Enter name or phone number"
       />
